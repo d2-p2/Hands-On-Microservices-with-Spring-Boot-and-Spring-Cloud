e61b8c1172dc1af456568fdfd02bcf09ba5ab48c
Dilendra <pant.dilendra@gmail.com>
2023-01-31 18:19:12 +0530

    Docker file changes

--- Chapter04/microservices/product-service/src/main/java/se/magnus/microservices/core/product/services/ProductServiceImpl.java
+++ Chapter04/microservices/product-service/src/main/java/se/magnus/microservices/core/product/services/ProductServiceImpl.java
@@ -3,6 +3,7 @@ package se.magnus.microservices.core.product.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import se.magnus.api.core.product.Product;
 import se.magnus.api.core.product.ProductService;
@@ -11,6 +12,7 @@ import se.magnus.util.exceptions.NotFoundException;
 import se.magnus.util.http.ServiceUtil;
 
 @RestController
+@ResponseBody
 public class ProductServiceImpl implements ProductService {
 
     private static final Logger LOG = LoggerFactory.getLogger(ProductServiceImpl.class);
